8133519ff92babc34ca3fe70d3f5c655c01522e2
zkza <zekuza909@gmail.com>
2021-03-29 13:09:57 +1100

    reprogrammed game into object

--- search/main.py
+++ search/main.py
@@ -8,13 +8,14 @@ This script contains the entry point to the program (the code in
 
 import sys
 import json
+from search.extra_utils import *
 # If you want to separate your code into separate files, put them
 # inside the `search` directory (like this one and `util.py`) and
 # then import from them like this:
 from search.util import print_board, print_slide, print_swing
 
-def main(filename):
 
+def main(filename):
     """
     try:
         with open(sys.argv[1]) as file:
@@ -31,20 +32,35 @@ def main(filename):
         print("usage: python3 -m search path/to/input.json", file=sys.stderr)
         sys.exit(1)
 
-    ## Convert the JSON into a board_dict
-    board_dict = {}
-    for hex in data['upper']:
-        token, p, q = hex
-        if token in ['r','p','s']:
-            token = token.upper()
-        board_dict[(p, q)] = token
+    # initialise game object
+    game = RoPaSciState()
 
-    for hex in data['lower']:
-        token, p, q = hex
-        board_dict[(p, q)] = token
+    # load the initial game state into the object
+    game.initialise(data=data)
 
-    print(board_dict)
-    print_board(board_dict)
+    heap = heapdatastructure
+    heap.insertkey(game,game.cost)
+    while heap is not empty:
+        for move in game.possible_moves:
+            game.copy()
+            game.apply_move(move)
+            heap.insertkey(game, game.cost)
+
+
+    #
+    # Pseudo-ish code
+    # initialise priority queue
+    # input board state into search problem
+    # while priority queue is not empty:
+    # pop min priority queue board state
+    # for each token
+    # output each possible move, and corresponding heuristic cost
+    # combine individual token moves into single moves
+    # insert into priority queue
+
+    # HEURISTIC:
+    # Sum up for each token, calculate the sum of square root distance from the token
+    # to all the opposing teams tokens it can defeat.
 
     # TODO:
     # Find and print a solution to the board configuration described
@@ -52,5 +68,6 @@ def main(filename):
     # Why not start by trying to print this configuration out using the
     # `print_board` helper function? (See the `util.py` source code for
     # usage information).
+
 
 main('test.json')
